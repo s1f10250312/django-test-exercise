1c039906e5bb23e2d1f887a41b3f296a82a142db
1F10250312 <s1f102503127@iniad.org>
2026-06-18 11:54:51 +0900

    Tskモデルにメソッドを追加

--- todo/models.py
+++ todo/models.py
@@ -8,3 +8,7 @@ class Task(models.Model):
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
     
+    def is_overdue(self, dt):
+        if self.due_at is None:
+            return False
+        return self.due_at < dt
