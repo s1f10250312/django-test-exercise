1b63a6acdebf369f611a559ff6ab29503955035f
1F10250312 <s1f102503127@iniad.org>
2026-06-25 12:09:02 +0900

    詳細ページを追加

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from todo import views as todo_views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', todo_views.index, name='index'),
+    path('<int:task_id>/', todo_views.detail, name='detail'),
 ]
